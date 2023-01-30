6f955ae510ad9655a71199f58ee1894986919cb5
Juan Pablo <juanpasan2000@gmail.com>
2023-01-29 21:02:11 -0500

    Project structure

---
+++ src/main/java/edu/eci/arep/HttpConnection.java
@@ -0,0 +1,44 @@
+package edu.eci.arep;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpConnection {
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String API_KEY = "c2d09dcc";
+    private static final String GET_URL = "http://www.omdbapi.com/?s=%1$s&apikey=%2$s";
+
+    public static void main(String[] args) throws IOException {
+        String movieName = "300";
+        String url = String.format(GET_URL, movieName, API_KEY);
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        System.out.println("GET DONE");
+    }
+}
