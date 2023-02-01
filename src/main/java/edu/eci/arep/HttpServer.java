13c77be015c552551d2a4fd552e12fcf6d52c9a7
Juan Pablo <juanpasan2000@gmail.com>
2023-01-31 20:04:20 -0500

    API connection with web server

--- src/main/java/edu/eci/arep/HttpServer.java
+++ src/main/java/edu/eci/arep/HttpServer.java
@@ -2,12 +2,17 @@ package edu.eci.arep;
 
 import java.net.*;
 import java.io.*;
+import java.util.HashMap;
 
+/**
+ * Web Server that uses an external API to find information about a specific movie
+ */
 public class HttpServer {
 
     private static final String USER_AGENT = "Mozilla/5.0";
     private static final String API_KEY = "c2d09dcc";
-    private static final String GET_URL = "http://www.omdbapi.com/?s=%1$s&apikey=%2$s";
+    private static final String GET_URL = "http://www.omdbapi.com/?t=%1$s&apikey=%2$s";
+    private static final HashMap<String, String> cache = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
@@ -35,15 +40,27 @@ public class HttpServer {
                             clientSocket.getInputStream()));
             String inputLine, outputLine;
 
+            boolean status = true;
+            String query = "";
             while ((inputLine = in.readLine()) != null) {
+                if (status) {
+                    query = inputLine.split(" ")[1];
+                    status = false;
+                }
                 System.out.println("Received: " + inputLine);
+                //System.out.println("-------" + request + "----------");
                 if (!in.ready()) {
                     break;
                 }
             }
-            outputLine = "HTTP/1.1 200 OK\r\n"
-                    + "\r\n"
-                    + htmlGetForm();
+
+            if (query.startsWith("/movie")) {
+                String movieName = query.split("name=")[1];
+                outputLine = htmlSimple(movieName);
+            } else {
+                outputLine = htmlGetForm();
+            }
+
             out.println(outputLine);
 
             out.close();
@@ -53,51 +70,71 @@ public class HttpServer {
         serverSocket.close();
     }
 
+    /**
+     * Method that finds the movie the user is looking for
+     * @param movieName movie's name
+     * @return String information of the movie in a Json format
+     * @throws IOException
+     */
     public static String searchMovie(String movieName) throws IOException {
-        String movie = "";
-        String url = String.format(GET_URL, movieName, API_KEY);
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        //The following invocation perform the connection implicitly before getting the code
-        int responseCode = con.getResponseCode();
-        System.out.println("GET Response Code :: " + responseCode);
-
-        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            movie = response.toString();
+        String movieJson = "";
+        if (cache.containsKey(movieName)) {
+            movieJson = cache.get(movieName);
+            System.out.println("Pelicula en cache");
         } else {
-            System.out.println("GET request not worked");
+            String url = String.format(GET_URL, movieName, API_KEY);
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            //The following invocation perform the connection implicitly before getting the code
+            int responseCode = con.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                movieJson = response.toString();
+            } else {
+                System.out.println("GET request not worked");
+            }
+            System.out.println("GET DONE");
+            cache.put(movieName, movieJson);
+            System.out.println("Guardada en cache");
         }
-        System.out.println("GET DONE");
-        return movie;
+        return movieJson;
     }
 
-    public static String htmlSimple() {
-        return "<!DOCTYPE html>"
-                + "<html>"
-                + "<head>"
-                + "<meta charset=\"UTF-8\">"
-                + "<title>Title of the document</title>\n"
-                + "</head>"
-                + "<body>"
-                + "My Web Site"
-                + "</body>"
-                + "</html>";
+    /**
+     * Method that displays in a json format the movie the user typed in the form
+     * @param movie movie name
+     * @return String according to the movie
+     * @throws IOException
+     */
+    public static String htmlSimple(String movie) throws IOException {
+        return "HTTP/1.1 200 OK\r\n" +
+                "Content-type: application/json\r\n" +
+                "\r\n" +
+                searchMovie(movie);
     }
 
+    /**
+     * Method that displays a get form
+     * @return String
+     */
     public static String htmlGetForm() {
-        return "<!DOCTYPE html>\n" +
+        return "HTTP/1.1 200 OK\r\n" +
+                "Content-type: text/html\r\n" +
+                "\r\n" +
+                "<!DOCTYPE html>\n" +
                 "<html>\n" +
                 "    <head>\n" +
                 "        <title>Form Example</title>\n" +
@@ -106,7 +143,7 @@ public class HttpServer {
                 "    </head>\n" +
                 "    <body>\n" +
                 "        <h1>Form with GET</h1>\n" +
-                "        <form action=\"/hello\">\n" +
+                "        <form action=\"/movie\">\n" +
                 "            <label for=\"name\">Name:</label><br>\n" +
                 "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
                 "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
@@ -121,11 +158,11 @@ public class HttpServer {
                 "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
                 "                    this.responseText;\n" +
                 "                }\n" +
-                "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
+                "                xhttp.open(\"GET\", \"/movie?name=\"+nameVar);\n" +
                 "                xhttp.send();\n" +
                 "            }\n" +
                 "        </script>\n" +
-                "\n" +
+                "\n" +/*
                 "        <h1>Form with POST</h1>\n" +
                 "        <form action=\"/hellopost\">\n" +
                 "            <label for=\"postname\">Name:</label><br>\n" +
@@ -143,7 +180,7 @@ public class HttpServer {
                 "                    .then(x => x.text())\n" +
                 "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n" +
                 "            }\n" +
-                "        </script>\n" +
+                "        </script>\n" +*/
                 "    </body>\n" +
                 "</html>";
     }
